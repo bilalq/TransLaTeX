4a7186d86f9e02dfed977b3baab065cf2d04d756
Bilal Quadri <bilalquadri92@gmail.com>
2012-09-15 16:04:25 -0400

    Fixed issues with file layout

---
+++ translatex.gemspec
@@ -0,0 +1,7 @@
+Gem::Specification.new do |s|
+  s.name = "ratex"
+  s.version = "0.0.1"
+  #s.add_runtime_dependency "racc",
+    #["= 1.4.9"]
+  #s.add_runtime_dependency "rex",
+    #["= 1.0.2"]
--- translatex.rb
+++
@@ -1,7 +0,0 @@
-Gem::Specification.new do |s|
-  s.name = "ratex"
-  s.version = "0.0.1"
-  #s.add_runtime_dependency "daemons",
-    #["= 1.1.0"]
-  #s.add_development_dependency "bourne",
-    #[">= 0"]
